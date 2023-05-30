ec2719c51bf1285123cbe8f158197e6d60f97e5f
Cadregich <theloyworkmail@gmail.com>
2023-05-31 00:20:14 +0300

    Navbar dropdown moves to left block with low screen width & center links

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,11 +1,12 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="d-flex flex-wrap w-100 px-1 px-lg-4 px-md-2">
-            <router-link to="/" class="navbar-brand order-0">AtomicCraft</router-link>
             <!-- Always visible left menu -->
-            <ul class="always-visible-left-menu navbar-nav">
-
+            <ul class="always-visible-left-menu navbar-nav d-flex flex-wrap">
+                <router-link to="/" class="navbar-brand order-0">AtomicCraft</router-link>
+                <!-- Содержимое always-visible-left-menu -->
             </ul>
+
             <!-- /.Always visible left menu -->
             <!-- Left menu -->
             <div class="left-menu collapse navbar-collapse order-2" id="navbarSupportedContent">
@@ -67,9 +68,15 @@
                     </div>
                 </div>
                 <!-- Burger -->
-                <button class="navbar-toggler ml-auto" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                <button
+                    class="navbar-toggler ml-auto"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <!-- /.Burger -->
@@ -80,29 +87,49 @@
 </template>
 
 <script>
-import {Dropdown} from "bootstrap";
+import { Dropdown } from "bootstrap";
 
 export default {
     name: "Navbar",
     data() {
         return {
             auth: true,
-        }
+            isRightLinkInLeftMenu: false,
+        };
+    },
+    methods: {
+        replaceItemsDOM(size) {
+            if (size < 400) {
+                const destination = document.getElementsByClassName("always-visible-left-menu")[0];
+                const block = document.getElementsByClassName("dropdown")[0];
+                destination.appendChild(block);
+                this.isRightLinkInLeftMenu = true;
+            } else {
+                const destination = document.getElementsByClassName("right-menu")[0];
+                const block = document.getElementsByClassName("dropdown")[0];
+                const firstChild = destination.firstChild;
+                destination.insertBefore(block, firstChild);
+            }
+        },
     },
     mounted() {
-        const element = document.getElementsByClassName('dropdown')[0];
-        const elementMenu = document.getElementsByClassName('dropdown-menu')[0];
+        window.addEventListener("resize", () => {
+            this.replaceItemsDOM(window.innerWidth);
+        });
+        this.replaceItemsDOM(window.innerWidth);
+        const element = document.getElementsByClassName("dropdown")[0];
+        const elementMenu = document.getElementsByClassName("dropdown-menu")[0];
         const dropdown = new Dropdown(element);
 
         function handleClick() {
             dropdown.show();
-            elementMenu.style.display = 'flex';
-            element.removeEventListener('click', handleClick);
+            elementMenu.style.display = "flex";
+            element.removeEventListener("click", handleClick);
 
             function resetClick() {
                 dropdown.hide();
-                elementMenu.style.display = 'none';
-                element.addEventListener('click', handleClick);
+                elementMenu.style.display = "none";
+                element.addEventListener("click", handleClick);
             }
 
             function handleClickOutside(event) {
@@ -111,26 +138,26 @@ export default {
                 }
             }
 
-            element.addEventListener('click', resetClick);
-            document.addEventListener('click', handleClickOutside);
+            element.addEventListener("click", resetClick);
+            document.addEventListener("click", handleClickOutside);
         }
 
-        element.addEventListener('click', handleClick);
+        element.addEventListener("click", handleClick);
 
-        element.addEventListener('mouseenter', function () {
+        element.addEventListener("mouseenter", function () {
             dropdown.show();
-            elementMenu.style.display = 'flex';
+            elementMenu.style.display = "flex";
         });
-        element.addEventListener('mouseleave', function () {
+        element.addEventListener("mouseleave", function () {
             dropdown.hide();
-            elementMenu.style.display = 'none';
+            elementMenu.style.display = "none";
         });
-        element.addEventListener('mouseleave', function () {
+        element.addEventListener("mouseleave", function () {
             dropdown.hide();
-            elementMenu.style.display = 'none';
+            elementMenu.style.display = "none";
         });
-    }
-}
+    },
+};
 </script>
 
 <style scoped>
@@ -163,7 +190,7 @@ export default {
 
 .dropdown-toggle:hover {
     color: black;
-    transition: .2s;
+    transition: 0.2s;
 }
 
 .dropdown-menu {
@@ -173,5 +200,16 @@ export default {
 
 #navbar-auth-block {
     margin-left: auto;
+}
+
+@media (max-width: 500px) {
+    .left-menu,
+    #navbar-auth-block {
+        justify-content: center;
+        text-align: center;
+    }
+    .left-menu {
+        margin: 0.5rem 0;
+    }
 }
 </style>
